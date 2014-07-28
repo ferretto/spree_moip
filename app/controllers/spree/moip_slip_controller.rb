77d99b7ab4a8c5d6ffbc53bd4bfcdb88dc5e8811
Marcos Teixeira <m.viniteixeira@gmail.com>
2014-07-27 23:40:58 -0300

    fix nasp

--- app/controllers/spree/moip_slip_controller.rb
+++ app/controllers/spree/moip_slip_controller.rb
@@ -1,17 +1,18 @@
 module Spree
   class MoipSlipController < Spree::StoreController
     skip_before_action :verify_authenticity_token, only: :notification
-    before_filter :set_order
+    before_filter :set_order, :only => [:generate_token]
     respond_to :json, :only => [:generate_token]
 
     def notification
-      payment = Spree::Payment.find_by_identifier(params[:id_transacao].split("_").first)
+      order = Spree::Order.find_by_number(params[:id_transacao].split("_").first)
+      payment = order.payments.joins(:payment_method).where(:spree_payment_methods => {:type => 'Spree::PaymentMethod::MoipSlip' }).first
       payment.started_processing!
 
       if params[:status_pagamento].eql?('4') && (payment.amount * 100).to_i.to_s.eql?(params[:valor])
         logger.info "[MOIP] Order #{@order.number} approved"
         payment.complete!
-        @order.next
+        order.next
       else
         logger.info "[MOIP] Order #{@order.number} failed"
         payment.pend!
@@ -27,7 +28,7 @@ module Spree
     private
     def set_order
       @order = current_order
-      @payment_method = Spree::PaymentMethod.find(params[:payment_method_id]) if params[:payment_method_id]
+      @payment_method = Spree::PaymentMethod.find(params[:payment_method_id])
     end
   end
 end
